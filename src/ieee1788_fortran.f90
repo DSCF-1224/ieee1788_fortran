4be6155a1c21cecce22f157a07fb6a36a299fdd0
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-04-24 06:46:02 +0900

    Update src & test files

--- src/ieee1788_fortran.f90
+++ src/ieee1788_fortran.f90
@@ -1,2 +1,8 @@
 module ieee1788_fortran
+
+    use, non_intrinsic :: ieee_class_fortran, only: is_ieee_negative_inf
+    use, non_intrinsic :: ieee_class_fortran, only: is_ieee_negative_zero
+    use, non_intrinsic :: ieee_class_fortran, only: is_ieee_positive_inf
+    use, non_intrinsic :: ieee_class_fortran, only: is_ieee_positive_zero
+
 end module ieee1788_fortran
