428e63982416ce62699cd1be1d3d9e7a3f9385e0
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-04-25 06:54:46 +0900

    Update src files Add accessibility setting

--- src/ieee1788_fortran.f90
+++ src/ieee1788_fortran.f90
@@ -9,4 +9,15 @@ module ieee1788_fortran
     use, non_intrinsic :: ieee_class_fortran, only: is_ieee_positive_inf
     use, non_intrinsic :: ieee_class_fortran, only: is_ieee_positive_zero
 
+
+    implicit none
+
+
+    private
+
+    public :: is_ieee_negative_inf
+    public :: is_ieee_negative_zero
+    public :: is_ieee_positive_inf
+    public :: is_ieee_positive_zero
+
 end module ieee1788_fortran
