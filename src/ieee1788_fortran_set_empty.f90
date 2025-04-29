7da98e9e8108424b23601ba7083d13e2baa66e40
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-04-29 21:37:34 +0900

    update `subroutine`s : `set_empty` & `set_entire` - introduce `set_ieee_negative_inf` - introduce `set_ieee_positive_inf`

--- src/ieee1788_fortran_set_empty.f90
+++ src/ieee1788_fortran_set_empty.f90
@@ -1,8 +1,7 @@
 submodule (ieee1788_fortran) ieee1788_fortran_set_empty
 
-    use, intrinsic :: ieee_arithmetic, only: ieee_negative_inf
-    use, intrinsic :: ieee_arithmetic, only: ieee_positive_inf
-    use, intrinsic :: ieee_arithmetic, only: ieee_value
+    use, non_intrinsic :: ieee_class_fortran, only: set_ieee_negative_inf
+    use, non_intrinsic :: ieee_class_fortran, only: set_ieee_positive_inf
 
     implicit none
 
@@ -12,24 +11,24 @@ submodule (ieee1788_fortran) ieee1788_fortran_set_empty
 
     module procedure bare_infsup_real32_set_empty
 
-        x%inf = ieee_value( x = x%inf, class = ieee_positive_inf )
-        x%sup = ieee_value( x = x%sup, class = ieee_negative_inf )
+        call set_ieee_positive_inf(x%inf)
+        call set_ieee_negative_inf(x%sup)
 
     end procedure bare_infsup_real32_set_empty
 
 
     module procedure bare_infsup_real64_set_empty
 
-        x%inf = ieee_value( x = x%inf, class = ieee_positive_inf )
-        x%sup = ieee_value( x = x%sup, class = ieee_negative_inf )
+        call set_ieee_positive_inf(x%inf)
+        call set_ieee_negative_inf(x%sup)
 
     end procedure bare_infsup_real64_set_empty
 
 
     module procedure bare_infsup_real128_set_empty
 
-        x%inf = ieee_value( x = x%inf, class = ieee_positive_inf )
-        x%sup = ieee_value( x = x%sup, class = ieee_negative_inf )
+        call set_ieee_positive_inf(x%inf)
+        call set_ieee_negative_inf(x%sup)
 
     end procedure bare_infsup_real128_set_empty
 
