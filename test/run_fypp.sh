de48523d2873f70a43669f4a3b99be24b1986c6b
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-04 09:55:51 +0900

    Add test : `set_entire`

--- test/run_fypp.sh
+++ test/run_fypp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
-fypp -F test_is_empty.fypp  test_is_empty.f90
-fypp -F test_kind.fypp      test_kind.f90
-fypp -F test_set_empty.fypp test_set_empty.f90
+fypp -F test_is_empty.fypp   test_is_empty.f90
+fypp -F test_kind.fypp       test_kind.f90
+fypp -F test_set_empty.fypp  test_set_empty.f90
+fypp -F test_set_entire.fypp test_set_entire.f90
