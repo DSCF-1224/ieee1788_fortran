d638c95693db55765687612f72af3747792a9291
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-04 21:16:09 +0900

    Add test : `is_entire`

--- test/run_fypp.sh
+++ test/run_fypp.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 fypp -F test_is_empty.fypp   test_is_empty.f90
+fypp -F test_is_entire.fypp  test_is_entire.f90
 fypp -F test_kind.fypp       test_kind.f90
 fypp -F test_set_empty.fypp  test_set_empty.f90
 fypp -F test_set_entire.fypp test_set_entire.f90
---
+++ test/test_is_entire.f90
@@ -0,0 +1,82 @@
+program test_is_entire
+
+    use, intrinsic :: iso_fortran_env, only: compiler_options
+    use, intrinsic :: iso_fortran_env, only: compiler_version
+
+
+    use, non_intrinsic :: ieee1788_fortran, only: bare_infsup_real32_type
+    use, non_intrinsic :: ieee1788_fortran, only: bare_infsup_real64_type
+    use, non_intrinsic :: ieee1788_fortran, only: bare_infsup_real128_type
+    use, non_intrinsic :: ieee1788_fortran, only: is_entire
+    use, non_intrinsic :: ieee1788_fortran, only: set_empty
+    use, non_intrinsic :: ieee1788_fortran, only: set_entire
+
+
+    implicit none
+
+
+    print * , compiler_version()
+    print * , compiler_options()
+
+
+    block
+
+        type(bare_infsup_real32_type) :: bare_infsup_real32
+        type(bare_infsup_real64_type) :: bare_infsup_real64
+        type(bare_infsup_real128_type) :: bare_infsup_real128
+
+
+        call set_empty(bare_infsup_real128)
+        call set_empty(bare_infsup_real128)
+        call set_empty(bare_infsup_real128)
+
+
+        if ( is_entire(bare_infsup_real128) ) then
+            error stop '; NG: is_entire(bare_infsup_real128 called set_empty) must be .false.'
+        end if
+
+
+        if ( is_entire(bare_infsup_real128) ) then
+            error stop '; NG: is_entire(bare_infsup_real128 called set_empty) must be .false.'
+        end if
+
+
+        if ( is_entire(bare_infsup_real128) ) then
+            error stop '; NG: is_entire(bare_infsup_real128 called set_empty) must be .false.'
+        end if
+
+    end block
+
+
+    block
+
+        type(bare_infsup_real32_type) :: bare_infsup_real32
+        type(bare_infsup_real64_type) :: bare_infsup_real64
+        type(bare_infsup_real128_type) :: bare_infsup_real128
+
+
+        call set_entire(bare_infsup_real128)
+        call set_entire(bare_infsup_real128)
+        call set_entire(bare_infsup_real128)
+
+
+        if ( .not. is_entire(bare_infsup_real128) ) then
+            error stop '; NG: is_entire(bare_infsup_real128 called set_entire) must be .false.'
+        end if
+
+
+        if ( .not. is_entire(bare_infsup_real128) ) then
+            error stop '; NG: is_entire(bare_infsup_real128 called set_entire) must be .false.'
+        end if
+
+
+        if ( .not. is_entire(bare_infsup_real128) ) then
+            error stop '; NG: is_entire(bare_infsup_real128 called set_entire) must be .false.'
+        end if
+
+    end block
+
+
+    print '(A)', 'OK: test_is_entire.fypp'
+
+end program test_is_entire
