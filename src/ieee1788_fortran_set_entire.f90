9bf43d2e06483c69aa9d437a98d21c6daff0e11b
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-05 01:06:12 +0000

    Deploying to github-pages from @ DSCF-1224/ieee1788_fortran@71fffa0008a4b50224fb6f79d7e963d950b94a98 🚀

--- src/ieee1788_fortran_set_entire.f90
+++ src/ieee1788_fortran_set_entire.f90
@@ -12,24 +12,24 @@ submodule (ieee1788_fortran) ieee1788_fortran_set_entire
 
     module procedure bare_infsup_real32_set_entire
 
-        call set_ieee_negative_inf(x%inf)
-        call set_ieee_positive_inf(x%sup)
+        call set_ieee_negative_inf(x%lower_bound)
+        call set_ieee_positive_inf(x%upper_bound)
 
     end procedure bare_infsup_real32_set_entire
 
 
     module procedure bare_infsup_real64_set_entire
 
-        call set_ieee_negative_inf(x%inf)
-        call set_ieee_positive_inf(x%sup)
+        call set_ieee_negative_inf(x%lower_bound)
+        call set_ieee_positive_inf(x%upper_bound)
 
     end procedure bare_infsup_real64_set_entire
 
 
     module procedure bare_infsup_real128_set_entire
 
-        call set_ieee_negative_inf(x%inf)
-        call set_ieee_positive_inf(x%sup)
+        call set_ieee_negative_inf(x%lower_bound)
+        call set_ieee_positive_inf(x%upper_bound)
 
     end procedure bare_infsup_real128_set_entire
 
